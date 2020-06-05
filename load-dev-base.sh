a0e59ddd8062869a8263de0da18034e7cdc005dd
Hector <ba0344@gmail.com>
2020-06-05 13:28:30 +0200

    Initial Config + Mysql

---
+++ load-dev-base.sh
@@ -0,0 +1,20 @@
+#To run it: sh ./load-dev-base.sh
+#!/usr/bin/env bash
+
+# Removing possible local servers
+#sudo service apache2 stop  # Linux
+#sudo apachectl -k stop     # Mac
+
+# Fresh Installation
+#	docker rm -f $(docker stop $(docker ps -aq))
+#	docker volume rm $(docker volume ls)
+#	docker rmi $(docker image ls)
+#	docker system prune -a -f
+#	docker system df
+#	docker network create dev-base
+
+# Bring up deb-base containers
+cd ~/Dev/dev-base/
+docker-compose up -d --build
+
+echo "----------------------> Dev Base Ready! Let's have fun!"
